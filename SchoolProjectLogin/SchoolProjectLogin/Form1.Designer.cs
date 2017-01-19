d27efdd69f91fdae11f7aef8375382f4f4da655a
Swathi <gbswathi@gmail.com>
2017-01-19 09:45:18 +0530

    School app login form

---
+++ SchoolProjectLogin/SchoolProjectLogin/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace SchoolProjectLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RollNumber = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.StudentName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RollNumber
+            // 
+            this.RollNumber.AutoSize = true;
+            this.RollNumber.Location = new System.Drawing.Point(116, 142);
+            this.RollNumber.Name = "RollNumber";
+            this.RollNumber.Size = new System.Drawing.Size(124, 25);
+            this.RollNumber.TabIndex = 1;
+            this.RollNumber.Text = "RollNumber";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(318, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(318, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(229, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "LOGIN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(399, 233);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 33);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // StudentName
+            // 
+            this.StudentName.AutoSize = true;
+            this.StudentName.Location = new System.Drawing.Point(116, 81);
+            this.StudentName.Name = "StudentName";
+            this.StudentName.Size = new System.Drawing.Size(142, 25);
+            this.StudentName.TabIndex = 0;
+            this.StudentName.Text = "StudentName";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 359);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.RollNumber);
+            this.Controls.Add(this.StudentName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label RollNumber;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label StudentName;
+    }
+}
+
---
+++ SchoolProjectLogin/SchoolProjectLogin/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SchoolProjectLogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\sbalared\Documents\SchoolDataBase.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True");
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From StudentInfo where StudentName='" + textBox1.Text + "'and RollNumber = '" + textBox2.Text + "'",con) ;
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                Main ss = new Main();
+                ss.Show();
+            }
+            else
+            {
+                MessageBox.Show("PLease check your name and Rollnumber");
+            }
+             }
+
+         private void button2_Click(object sender, EventArgs e)
+         {
+             this.Close();
+                 
+         }
+
+       
+
+                
+    }
+}
